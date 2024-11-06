f020541cba2aea05639265e7ae4fb6318779d973
any <you@example.com>
2024-11-06 15:30:56 -0300

    add level in orm

--- TCCGames/models.py
+++ TCCGames/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    score = models.IntegerField(default=0)
+    score = models.FloatField(default=0)
+    level = models.IntegerField(default=0)
 
     def __str__(self):
         return self.username
